ef9d093e1e43a1b57dcc7c0de5bd31b9b68d02dd
Helen Du <helendu.614@gmail.com>
2018-01-04 23:12:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,8 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(names)
   names.collect {|name| badge_maker(name)}
-  end
 end
+
 
 def assign_rooms(names)
   names.collect.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
