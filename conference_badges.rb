4f9eb96895791b4f5aca9a23684620386d950211
Helen Du <helendu.614@gmail.com>
2018-01-04 22:59:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,35 +1,27 @@
-# Write your code here.
 def badge_maker(name)
-  return "Hello, my name is #{name.}"
-end
-
-#takes an array of names as an argument
-#returns an array of badge messages.
-names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def batch_badge_creator(attendees)
-  batch_badge_array = []
-  attendees.each do |name|
-    batch_badge_array.push("Hello, my name is #{name}.")
+    return "Hello, my name is #{name}."
+ end
+ def batch_badge_creator(attendees)
+   badge_messages=[]
+   attendees.each do |count|
+     badge_messages.push("Hello, my name is #{count}.")
+   end
+   return badge_messages
   end
-  batch_badge_array
-end
-
-def assign_rooms(attendees)
- +  room_assignments = []
- +  i = 1
- +  attendees.each do |name|
- +    room_assignments.push("Hello, #{name}! You'll be assigned to room #{i}!")
- +    i += 1
- +  end
- +  room_assignments
- +end
- +
- +def printer(attendees)
- +  batch_badge_creator(attendees).each do |index|
- +    puts index
- +  end
- +  assign_rooms(attendees).each do |index|
- +    puts index
- +  end
- +end
+ def assign_rooms(attendees)
+   assign_array=[]
+    attendees.each_with_index {
+     |value, index| assign_array.push("Hello, #{value}! You'll be assigned to room #{index1}!")
+   }
+   return assign_array
+ end
+ def printer(attendees)
+   count = 0
+   assign_array=[]
+   assign_array=assign_rooms(attendees)
+  while count < attendees.length
+    puts badge_maker(attendees[count])
+    puts assign_array[count]
+    count =1
+  end
+ end
