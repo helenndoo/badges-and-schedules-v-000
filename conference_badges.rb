4398d1aa85ae76c1d1fa75efcf9f88106d0b55cc
Helen Du <helendu.614@gmail.com>
2018-01-04 22:56:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,10 +7,12 @@ end
 #returns an array of badge messages.
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(names)
-  names.map do |name|
-    badge_maker(name)
+def batch_badge_creator(attendees)
+  batch_badge_array = []
+  attendees.each do |name|
+    batch_badge_array.push("Hello, my name is #{name}.")
   end
+  batch_badge_array
 end
 
 def assign_rooms(names)
